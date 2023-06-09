32b4fd9ae177c642b4def6cdf257a96f22247198
Arun Pariyar <arun.pariyar@gmail.com>
2023-06-09 20:12:18 +0200

    :clown_face: fix: added offer to mock data

--- server/mocks.js
+++ server/mocks.js
@@ -10,6 +10,7 @@ const mocks = [
     major chords and a few songs.
     looking forward to hearing from you`,
     interests: `Make an offer for other learnings`,
+    offer: "Teach",
   },
   {
     imageUrl: "https://i.pravatar.cc/150?u=2@pravatar.com",
@@ -17,6 +18,7 @@ const mocks = [
     message: `
     I have been practiing Qigong for quite a year now but was keen to learn some basic yoga and philosopy. Would be amazing if someone would like to have and exchange`,
     interests: `sorry but i am not taking other offers`,
+    offer: "Learn",
   },
   {
     imageUrl: "https://i.pravatar.cc/150?u=3@pravatar.com",
@@ -24,6 +26,7 @@ const mocks = [
     message: `
    I have been learning french for sometime now and would love to speak with a native speaker in exchange I am also happy to speak with you in english, its my mother tongue`,
     interests: `Sorry cant be flexible with other offers`,
+    offer: "Teach",
   },
   {
     imageUrl: "https://i.pravatar.cc/150?u=4@pravatar.com",
@@ -31,6 +34,7 @@ const mocks = [
     message: `
     I want to improve my backend skills and was keen to find someone who has a solid basic understanding of express. In return I am happy to teach you anything react related`,
     interests: `Happy to also exchange for devops lesson :)`,
+    offer: "Learn",
   },
 ];
 
--- server/router.js
+++ server/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mocks = require("./mocks");
 
 router.get("/explore", (req, res) => {
+  res.status(200);
   res.send({ data: mocks, error: null });
 });
 
