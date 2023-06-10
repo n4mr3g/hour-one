b34239f720a48c5f10b53c6bdb0dd7eac7db09f3
Arun Pariyar <arun.pariyar@gmail.com>
2023-06-10 12:39:39 +0200

    :bug: fix: navbar style error

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -4,21 +4,26 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export default function Navigation() {
-  const [listCount, setListCount] = useState(1);
+  const [listCount, setListCount] = useState(8);
   return (
     <div className="navigation-container">
       <div>Logo</div>
       <SearchBar />
       <div className="nav-options">
-        <Link to={"/"} className="my-list">
-          My List{" "}
-          <span className={listCount > 0 ? "list-count" : ""}>
+        <div className="mylist-wrapper">
+          <Link to={"/"} className="my-list">
+            My List
+          </Link>
+          <div className={listCount > 0 ? "list-count" : ""}>
             {listCount > 0 ? listCount : ""}
-          </span>
-        </Link>
-        <div>
-          <span className="material-symbols-outlined">account_circle</span>
+          </div>
         </div>
+
+        <img
+          className="user-image"
+          src={"https://i.pravatar.cc/150?50=fake@pravatar.com"}
+          alt="user"
+        />
       </div>
     </div>
   );
